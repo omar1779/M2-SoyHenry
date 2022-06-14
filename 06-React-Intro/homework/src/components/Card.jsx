41485f3a277034ee2596e19eb1b4f4b1d81e4ef9
omar1779 <josemyclans@gmail.com>
2022-06-14 00:06:47 -0500

    introduccion a React.js

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
+  //const = {name ,min , max , onClose ,img}
+  return (
+    <div>
+      <button onClick={props.onClose}>X</button>
+      <h4>{props.name}</h4>
+      <p>Min</p>
+      <p>{props.min}</p>
+      <p>max</p>
+      <p>{props.max}</p>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="" />
+    </div>
+  )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <input type="text" placeholder='Ciudades'/>
+      <button onClick={() => props.onSearch('buscando...')}>agregar</button>
+    </div>
+  )
 };
